5dbfa3e41bdbbb55f4853d0896e3ca96ba4f509e
mkambampati <166394698+mkambampati@users.noreply.github.com>
2025-02-15 19:38:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "aws_type" {
   description = "AWS ec2 type"
-  default = "t3.medium"
+  default = "t2.medium"
 }
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-053a45fff0a704a47"
 }
